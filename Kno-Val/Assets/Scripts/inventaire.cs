40494fdb6adead7c59d154a99d1c8d29aa6313d1
WilliamFoucher <foucher.william1@gmail.com>
2021-05-30 23:30:19 -0400

    Début inventaire + amasser objet
    c'est ça qui est ça

---
+++ Kno-Val/Assets/Scripts/inventaire.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventaire : MonoBehaviour
+{
+    bool inventaireOuvert = false;
+    public float longueurAmasser;
+
+    public Camera cameraJoueur;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.E) && inventaireOuvert == false)
+        {
+            OuvreInventaire();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && inventaireOuvert == true)
+        {
+            fermeInventaire();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AmasserObjet();
+        }
+    }
+
+    void OuvreInventaire()
+    {
+        inventaireOuvert = true;
+
+        print("inventaire Ouvert");
+
+    }
+
+    void fermeInventaire()
+    {
+        inventaireOuvert = false;
+        print("inventaire fermé");
+    }
+
+    void AmasserObjet()
+    {
+        RaycastHit amasser;
+        if (Physics.Raycast(cameraJoueur.transform.position, cameraJoueur.transform.forward, out amasser, longueurAmasser))
+        {
+            Debug.Log(amasser.transform.name);
+        }
+    }
+}
